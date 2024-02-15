b90b73b47474039739def7baea9949a0af5dc2b0
Benjamin Haase <benjaminthaase@protonmail.com>
2024-02-15 11:10:35 -0800

    Adds getting input from client and sending request for discrete logarithm

--- src/bin/interface/mod.rs
+++ src/bin/interface/mod.rs
@@ -1,8 +1,8 @@
-use std::io::{Read, Write, BufRead, stdout};
+use std::io::{Read, Write, BufRead, stdout, Stdin, Stdout};
 use std::time::Duration;
 use std::str::FromStr;
 use tokio::io::{AsyncWrite, AsyncWriteExt, AsyncRead, AsyncReadExt};
-use termion::{raw::IntoRawMode, color, style, cursor, input, clear};
+pub use termion::{raw::{IntoRawMode, RawTerminal}, color, style, cursor, input, clear};
 
 use discrete_log_server::{Response, BytesDeser, BytesSer, AsBytes, Frame};
 use super::ClientError;
@@ -37,16 +37,16 @@ impl Interface {
                 stdout.flush().map_err(|e| ClientError::Write(e))?;
                 // Display menu of options
                 write!(
-                    stdout, "{}{}{}\n{}\n{}\n{}\n",
-                    cursor::Goto(1, 2), color::Fg(color::Rgb(225, 247, 244)),
+                    stdout, "{}{}{}{}{}{}\n",
+                    cursor::Goto(1, 5), color::Fg(color::Rgb(225, 247, 244)),
                     "[q] - Quit", "[:p] - Check if p is prime", "[l] - Solve discrete logarithm", "[r] - Factor RSA public key"
                 ).map_err(|e| ClientError::Write(e))?;
                 stdout.flush().map_err(|e| ClientError::Write(e))?;
-                // Display prompt to client
-                write!(
-                    stdout, "{}{}", cursor::Goto(1, 6), ">>> "
-                ).map_err(|e| ClientError::Write(e))?;
-                stdout.flush().map_err(|e| ClientError::Write(e))?;
+                // // Display prompt to client
+                // write!(
+                //     stdout, "{}{}", cursor::Goto(1, 6), ">>> "
+                // ).map_err(|e| ClientError::Write(e))?;
+                // stdout.flush().map_err(|e| ClientError::Write(e))?;
                 Ok(Interface::Home)
             }
             Interface::Home => {
@@ -57,6 +57,7 @@ impl Interface {
     }
 
     pub async fn parse_request<W: AsyncWriteExt + Unpin, C: Read>(self, mut to_server: W, mut from_client: C) -> Result<Self, ClientError> {
+        let mut stdout = stdout().into_raw_mode().expect("unable to convert terminal into raw mode");
         match self {
             Interface::Home => {
                 let mut buf = String::default();
@@ -76,6 +77,17 @@ impl Interface {
                             .map_err(|e| ClientError::SendRequest(e))?;
                         return Ok(Interface::Prime);
                     }
+                    "l" => {
+                        let base = utils::read_u64("base", &mut from_client, &mut stdout)?;
+                        let val = utils::read_u64("value", &mut from_client, &mut stdout)?;
+                        let prime = utils::read_u64("prime", &mut from_client, &mut stdout)?;
+
+                        // create frame and send to server
+                        let frame = Frame::Log { g: base, h: val, p: prime };
+                        to_server.write_all(&frame.as_bytes())
+                            .await
+                            .map_err(|e| ClientError::SendRequest(e))?;
+                    }
                     _ => todo!()
                 }
 
@@ -84,5 +96,36 @@ impl Interface {
         }
 
         todo!()
+    }
+}
+
+
+mod utils {
+    use super::*;
+    pub fn read_u64<'a, C: Read>(label: &'a str, from_client: &mut C, out: &mut RawTerminal<Stdout>) -> Result<u64, ClientError> {
+        loop {
+            write!(
+                out, "{}{}{}",
+                cursor::Goto(1, 5), clear::CurrentLine, format!("enter {}: ", label),
+            ).map_err(|e| ClientError::Write(e))?;
+            out.flush().map_err(|e| ClientError::Write(e))?;
+
+            let mut buf = String::default();
+            from_client.read_to_string(&mut buf)
+                .map_err(|e| ClientError::Read(e))?;
+
+            match u64::from_str(buf.trim_end_matches('\n')) {
+                Ok(v) => return Ok(v),
+                Err(e) => {
+                    write!(
+                        out, "{}{}{}{}{}",
+                        cursor::Goto(1, 4), color::Fg(color::Rgb(242, 217, 104)),
+                        clear::CurrentLine, "please enter a valid unsigned integer",
+                        color::Fg(color::Reset)
+                    ).map_err(|e| ClientError::Write(e))?;
+                    out.flush().map_err(|e| ClientError::Write(e))?;
+                }
+            }
+        }
     }
 }
