6ca4c0efcc534225af84b4dbc28e5ad4f46304f1
Benjamin Haase <benjaminthaase@protonmail.com>
2024-02-18 11:40:39 -0800

    Adds Client struct

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,5 +1,8 @@
-use std::io;
+use std::io::{self, stdin, Read};
+use std::net::SocketAddr;
+use tokio::net::{TcpStream};
 use tokio::io as tokio_io;
+use tokio_io::{AsyncReadExt, AsyncWriteExt};
 use crate::interface::Interface;
 
 mod interface;
@@ -10,9 +13,42 @@ pub enum ClientError {
     Read(io::Error),
     SendRequest(tokio_io::Error),
     IllegalResponse,
-    InterfaceState(Interface)
+    InterfaceState(Interface),
+    Connection(io::Error),
 }
 
+struct Client;
+
+impl Client {
+    async fn connect(addr: SocketAddr) -> Result<(), ClientError> {
+        // create interface
+        let mut interface = Interface::new();
+
+        // handle to standard input
+        let mut stdin = stdin();
+
+        // connect to server
+        let server_socket = TcpStream::connect(addr)
+            .await
+            .map_err(|e| ClientError::Connection(e))?;
+        let (mut from_server, mut to_server) = server_socket.into_split();
+
+        // main loop for the ui
+        loop {
+            interface = interface.receive_response(&mut from_server).await?;
+            interface = match interface.parse_request(&mut to_server, &mut stdin).await {
+                Ok(Interface::Quit) => {
+                    // TODO: log exiting application
+                    break;
+                }
+                Ok(i) => i,
+                Err(e) => return Err(e),
+            };
+        }
+
+        Ok(())
+    }
+}
 fn main() {
     todo!()
 }
